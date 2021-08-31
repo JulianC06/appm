4ae43a55dda84dbd515495ed0754abbed1679640
JulianC06 <william.castaneda03@uptc.edu.co>
2021-08-31 17:43:57 -0500

    errores arreglados

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+const path = require('path');
 const express = require('express');
 const fs = require('fs');
 const cprocess = require('child_process');
 const filetxt1 = 'statusInstance1.txt';
 const filetxt2 = 'statusInstance2.txt';
-var arrayInstance1=[];
-var arrayInstance2=[];
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('<h1>Página de status del sitio</h1>');
+	//res.sendFile(path.resolve(__dirname, 'index.html'));
 
 	cprocess.exec('sh scriptStatus.sh', (err,stdout,stderr)=>{
 		if(err){
@@ -19,30 +18,37 @@ app.get('/', (req, res) => {
 			//Instancia 1
 			var contentInstance1 = fs.readFileSync(`./status/${filetxt1}`, 'UTF-8');
 			var byContentInstance1 = contentInstance1.split('\n');
-			var byStringInstance1 = byContentInstance1[byContentInstance1.length-1].split(';');
+			var byStringInstance1 = byContentInstance1[byContentInstance1.length-2].split(';');
 			var mostRecentStatusInstance1=byStringInstance1[1];
-			if(mostRecentStatusInstance1=='live'){
-				res.send('<h1>Instancia 1: <span style="color:green">viva</span></h1>');
-			}else{
-				res.send('<h1>Instancia 1: <span style="color:red">muerta</span></h1>');
-			}
+
 			//Instancia 2
 			var contentInstance2 = fs.readFileSync(`./status/${filetxt2}`, 'UTF-8');
 			var byContentInstance2 = contentInstance2.split('\n');
-			var byStringInstance2 = byContentInstance2[byContentInstance2.length-1].split(',');
+			var byStringInstance2 = byContentInstance2[byContentInstance2.length-2].split(',');
 			var mostRecentStatusInstance2=byStringInstance2[1];
 
-			if(mostRecentStatusInstance2=='live'){
-				res.send('<h1>Instancia 2: <span style="color:green">viva</span></h1>');
-			}else{
-				res.send('<h1>Instancia 2: <span style="color:muerta">viva</span></h1>');
+			if(mostRecentStatusInstance1=='live'&& mostRecentStatusInstance2=='live'){
+				res.send('<h1>Instancia 1: <span style="color:green">viva</span></h1><br><h1>Instancia 2: <span style="color:green">viva</span></h1>');
+			}else if(mostRecentStatusInstance1=='die' && mostRecentStatusInstance2=='die'){
+				res.send('<h1>Instancia 1: <span style="color:red">muerta</span></h1><br><h1>Instancia 2: <span style="color:red">muerta</span></h1>');
+			}else if(mostRecentStatusInstance1=='live' && mostRecentStatusInstance2=='die'){
+				res.send('<h1>Instancia 1: <span style="color:green">viva</span></h1><br><h1>Instancia 2: <span style="color:red">muerta</span></h1>');
+			}else if(mostRecentStatusInstance1=='die' && mostRecentStatusInstance2=='live'){
+				res.send('<h1>Instancia 1: <span style="color:red">muerta</span></h1><br><h1>Instancia 2: <span style="color:green">live</span></h1>');
 			}
 
 			
 		}
 	});
+
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+p=0
+while(p!=0){
+p;
+
+}
